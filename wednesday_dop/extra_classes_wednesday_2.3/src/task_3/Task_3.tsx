d483c0d7df3d581fe4d2f436992e21401904655d
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-07-09 13:59:36 +0300

    dop

--- wednesday_dop/extra_classes_wednesday_2.3/src/task_3/Task_3.tsx
+++ wednesday_dop/extra_classes_wednesday_2.3/src/task_3/Task_3.tsx
@@ -1,24 +1,97 @@
-import {ChangeEvent, useState} from 'react';
-import { SlowComponent } from './slowComponent/SlowComponent';
+import React, {ChangeEvent, memo, useCallback, useRef, useState} from 'react';
+import {SlowComponent} from './slowComponent/SlowComponent';
 
 
 //find the problem and fix it as part of composition optimization, memo, children
 
+/*
+//composition optimization
 export const Task_3 = () => {
-  const [value, setValue] = useState('');
-
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-      console.log('Task_3')
-      return (
-          setValue(event.currentTarget.value)
-      )
-}
-
   return (
     <div>
       <div>Lags when change value</div>
-      <input type="text" value={value} onChange={onChange} />
+      <Input/>
       <SlowComponent />
     </div>
   );
-};
+}
+export const Input = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('Task_3')
+        return (
+            setValue(event.currentTarget.value)
+        )
+    }
+    return <input type="text" value={value} onChange={onChange}/>
+}*/
+
+/*
+//children
+export const Task_3 = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('Task_3')
+        return (
+            setValue(event.currentTarget.value)
+        )
+    }
+
+    return (
+        <div>
+            <div>Lags when change value</div>
+            <input type="text" value={value} onChange={onChange}/>
+            <SlowComponentWrapper>
+                <SlowComponent/>
+            </SlowComponentWrapper>
+
+        </div>
+    );
+}
+type SlowComponentWrapperType = {
+    children: React.ReactNode
+}
+
+const SlowComponentWrapperMemo = memo(({ children }: SlowComponentWrapperType) => {
+    console.log('SlowComponentWrapper re-render...');
+    return (
+        <>
+            {children}
+        </>
+    )
+});
+
+export const SlowComponentWrapper = (props: SlowComponentWrapperType) => {
+    return <SlowComponentWrapperMemo {...props} />;
+};*/
+/*export const Task_3 = () => {
+    let Iref = useRef<HTMLInputElement>(null)
+    return (
+        <div>
+            <div>Lags when change value</div>
+            <input type="text" ref={Iref}/>
+            <SlowComponent/>
+        </div>
+    );
+}*/
+export const Task_3 = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('Task_3')
+        return (
+            setValue(event.currentTarget.value)
+        )
+    }
+
+    return (
+        <div>
+            <div>Lags when change value</div>
+            <input type="text" value={value} onChange={onChange}/>
+            <SlowComponent/>
+        </div>
+    );
+}
+
--- wednesday_dop/extra_classes_wednesday_2.3/src/task_3/slowComponent/SlowComponent.tsx
+++ wednesday_dop/extra_classes_wednesday_2.3/src/task_3/slowComponent/SlowComponent.tsx
@@ -1,6 +1,7 @@
-import {memo, useMemo} from "react";
+import {memo, useEffect, useMemo, useRef, useState} from "react";
 
-export const SlowComponent = memo(() => {
+//memo
+/*export const SlowComponent = memo(() => {
   console.log('SlowComponent re-render...');
 
   let now = performance.now();
@@ -10,4 +11,40 @@ export const SlowComponent = memo(() => {
   }
 
   return <p>I am a very slow component tree.</p>;
-})
+})*/
+/*export const SlowComponent = () => {
+    console.log('SlowComponent re-render...');
+    const [use, setUse] = useState(() => {
+        let now = performance.now();
+
+        while (performance.now() - now < 1000) {
+            // Artificial delay -- do nothing for 100ms
+        }
+    })
+    return <p>I am a very slow component tree.</p>;
+}*/
+/*export const SlowComponent = () => {
+  console.log('SlowComponent re-render...');
+  useEffect(() => {
+    let now = performance.now();
+
+    while (performance.now() - now < 1000) {
+      // Artificial delay -- do nothing for 100ms
+    }
+  }, []);
+  
+
+  return <p>I am a very slow component tree.</p>;
+}*/
+
+export const SlowComponent = () => {
+  console.log('SlowComponent re-render...');
+
+  let now = performance.now();
+
+  while (performance.now() - now < 1000) {
+    // Artificial delay -- do nothing for 100ms
+  }
+
+  return <p>I am a very slow component tree.</p>;
+}
